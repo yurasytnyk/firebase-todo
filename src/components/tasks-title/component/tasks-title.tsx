bc4b6a01b3af0da1c2b0001f9bd9945d885a635c
Yura Sytnyk <yurii.sytnyk@spd-ukraine.com>
2022-03-08 16:20:11 +0200

    add few dependencies

--- src/components/tasks-title/component/tasks-title.tsx
+++ src/components/tasks-title/component/tasks-title.tsx
@@ -1,11 +1,6 @@
-import { 
-  ChangeEvent,
-  FC, 
-  useState, 
-} from 'react';
+import { FC } from 'react';
 
 import { Props } from '../types/tasks-title-types';
-import EditIconSvg from '../../../assets/img/edit.svg';
 import '../styles/tasks-title-styles.scss';
 
 export const TasksTitle: FC<Props> = (props) => {
@@ -14,28 +9,12 @@ export const TasksTitle: FC<Props> = (props) => {
     color,
   } = props;
 
-  const [isDisabled, setIsDisabled] = useState(true);
-  const [value, setValue] = useState(title);
-
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
-
-  const onClickHandler = () => setIsDisabled(!isDisabled);
-
   return (
-    <div>
-      <input
-        className="tasks__title"
-        style={{ color: color }}
-        disabled={isDisabled}
-        value={value}
-        onChange={onChangeHandler}
-      />
-
-      <span onClick={onClickHandler}>
-        <img src={EditIconSvg} alt="edit icon" />
-      </span>
-    </div>
+    <h2
+      className="tasks__title"
+      style={{ color: color }}
+    >
+      {title}
+    </h2>
   );
 };
